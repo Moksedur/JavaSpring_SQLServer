9e0d77f7b4b30a5f59b7469b271f96d93f1c915a
Moksedur <moksedur_rahman@yahoo.com>
2018-11-17 18:12:45 +0600

    First Commit

---
+++ JavaSpring_SQLServer/src/main/java/com/datavisionbd/javaspring_sqlserver/JdbcMsSql.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.datavisionbd.javaspring_sqlserver;
+
+import java.sql.Connection;
+//import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Md. Moksedur Rahman
+ */
+public class JdbcMsSql {
+    	public static void main(String[] args) {
+
+        // Create a variable for the connection string.
+        String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=NitgenAccessManager;user=sa;password=sa";
+
+        try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+            String SQL = "SELECT * FROM NGAC_AUTHLOG";
+            ResultSet rs = stmt.executeQuery(SQL);
+System.out.println("In !");
+            // Iterate through the data in the result set and display it.
+            while (rs.next()) {
+//                System.out.println(rs.getString("UserIDIndex") + " " + rs.getString("TransactionTime"));
+System.out.println("Found !");
+            }
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ JavaSpring_SQLServer/src/main/java/com/datavisionbd/javaspring_sqlserver/Jdbcconnection.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.datavisionbd.javaspring_sqlserver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Md. Moksedur Rahman
+ */
+public class Jdbcconnection {
+    public void dbConnect(String db_connect_string,
+            String db_userid,
+            String db_password)
+   {
+      try {
+         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+         Connection conn = DriverManager.getConnection(db_connect_string,
+                  db_userid, db_password);
+         System.out.println("connected");
+         Statement statement = conn.createStatement();
+         String queryString = "select * from sysobjects where type='u'";
+         ResultSet rs = statement.executeQuery(queryString);
+         while (rs.next()) {
+            System.out.println(rs.getString(1));
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   public static void main(String[] args)
+   {
+      ConnectMSSQLServer connServer = new ConnectMSSQLServer();
+      connServer.dbConnect("jdbc:sqlserver://<hostname>", "<user>",
+               "<password>");
+   }
+}
